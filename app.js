95c867f91119669a50218c76604803f74b74eee1
Aruthea002 <zthwoo@gmail.com>
2024-09-06 14:17:05 +0800

    message

---
+++ app.js
@@ -0,0 +1,62 @@
+// Data types
+//String
+
+let name = "Thea";
+console.log("Your name: " + name);
+let age = 20;
+console.log ("Your age: " + age);
+
+ 
+
+//Arithmetic and Logical Operators
+
+let a = 500;
+let b = 20;
+let c = 30;
+let diff = a + b * c;
+console.log("My grades: " + diff);
+
+
+//Conditional Statement
+
+if(age>=200){
+    console.log("bagsak Ka ");
+}else;
+    console.log(" Pasar ka ");
+
+
+    let grado = 80;
+    let category;
+
+    switch(true){
+        case (grado>=200):
+            category = "Pasar ka ni lapas pa heaven imong grado ";
+            break;
+            default:
+                category = "Bagsak ka loya jud nmo";
+                break;
+
+                console.log("Naka survive pa");
+
+                //loops
+
+                const activities = ["study","tulog","kaon"];
+                for (let w = 0; w < activities.length; w++){
+                    console.assert;ongamepadconnected('activities ${w + 1}: ${activities[w]}');
+
+                }
+}
+// Functional Programming
+// Determine odd or even
+
+function CheckNumber(Number){
+    const numberToCheck = 30;
+    if (number % 2 === 0){
+        console.log('${number} is even.');
+    }else if (isPrime(number)) {
+        console.log('${number} is prime.');
+    }else{
+        console.log('${number} is niether even nor prime.');
+    }
+    }
+    console.log("the number is prime");
